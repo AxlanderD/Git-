e90ad89c5b025acb6273c4dcc80de5782442bf31
DD4 <3288307510@qq.com>
2020-06-19 09:00:18 +0800

    创建初始文件-0619

---
+++ com.dwj/starter.java
@@ -0,0 +1,11 @@
+public class starter{
+
+    static STATUS suc = STATUS.SUCCESS;
+    static STATUS fail = STATUS.FAIL;
+
+}
+
+public enum STATUS{
+    SUCCESS(0),
+    FAIL(1);
+}
---
+++ com.dwj/ticketStolen.java
@@ -0,0 +1,8 @@
+package com.dwj;
+
+
+public class ticketStolen{
+    public static void main(String[] args){
+        
+    }
+}
